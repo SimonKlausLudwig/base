ba51ba4429c2bab786eb9d1a42521e8d65723e44
Your Name <you@example.com>
2023-01-02 23:07:21 +0000

    push

--- copy.sh
+++ copy.sh
@@ -1,12 +1,19 @@
 cd ..
 rm -rf html-temp
-cp -r html/ html-temp/
+mkdir html-temp
+cd html-temp
+git init
+git remote add origin git@github.com:SimonKlausLudwig/base.git
+git branch -M main
+git pull origin main
+
+cp -r ../html/* ./
 cd html-temp
 rm -rf .git
 find . -type f -iname '*loesung*' -delete
 git init
 git remote add origin git@github.com:SimonKlausLudwig/base.git
-git branch -M main
-git add -a
+git add -A
 git commit -m "push"
+git branch -M main
 git push -u origin main
